8ceb4ad630147e5a0f58052adee5b3e9f5ac4ba7
Jelle Vandebeeck <jelle@fousa.be>
2018-12-30 12:17:37 +0100

    Add public keys to the configuration.

--- Sources/Configuration/Configuration.swift
+++ Sources/Configuration/Configuration.swift
@@ -5,10 +5,15 @@
 //  Created by Jelle Vandebeeck on 01/12/2018.
 //
 
+/// A nicer way of defining the mapping between host and public keys.
+public typealias PublicKeys = [String: String]
+
 /// The configuration is used by the `Service` instance during the execution of the request.
 public protocol Configuration {
     /// The base url that is appended to the `Request`'s relative url.
     var baseURL: URL? { get }
     /// Set the headers that will be used for all the requests.
     var headers: RequestHeaders? { get }
+    /// Set the public keys for the hosts.
+    var publicKeys: PublicKeys? { get }
 }
