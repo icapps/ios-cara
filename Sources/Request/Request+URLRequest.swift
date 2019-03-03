414217c00f62ce8f7c66f86f8cdc11c55a6cd909
Jelle Vandebeeck <jelle@fousa.be>
2019-03-03 09:39:19 +0100

    Correctly append the query to an existing query.

--- Sources/Request/Request+URLRequest.swift
+++ Sources/Request/Request+URLRequest.swift
@@ -21,13 +21,31 @@ extension Request {
     private func makeURL(with configuration: Configuration) throws -> URL {
         // A correct url should be set. And when this is the case we alreay try to append the query
         // to this url.
-        guard let url = url else { throw ResponseError.invalidURL }
-        // When the url is an absolure url, just use this url.
-        guard url.host == nil else { return url.appendingQuery(from: self) }
-        // When the url is a relative url we should have a base url defined.
-        guard let baseURL = configuration.baseURL else { throw ResponseError.invalidURL }
+        guard
+            let url = url,
+            let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else {
+            throw ResponseError.invalidURL
+        }
+        
+        var requestURL: URL
+        if components.host != nil {
+            // When the url is an absolure url, just use this url.
+            requestURL = url
+        } else if let baseURL = configuration.baseURL {
+            // When the url is a relative url we should have a base url defined.
+            let newURL = baseURL.appendingPathComponent(components.path)
+            requestURL = newURL
+        } else {
+            throw ResponseError.invalidURL
+        }
+        
+        // Append the original query paramters to the new request url.
+        var requestComponents = URLComponents(url: requestURL, resolvingAgainstBaseURL: false)
+        requestComponents?.queryItems = components.queryItems
+        guard let correctURL = requestComponents?.url else { throw ResponseError.invalidURL }
+        
         // Return the relative url appended to the base url.
-        return baseURL.appendingPathComponent(url.absoluteString).appendingQuery(from: self)
+        return correctURL.appendingQuery(from: self)
     }
     
     private func makeHeaders(with configuration: Configuration) -> RequestHeaders? {
@@ -53,8 +71,18 @@ extension Request {
 
 fileprivate extension URL {
     func appendingQuery(from request: Request) -> URL {
-        guard var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: false) else { return self }
-        urlComponents.queryItems = request.query?.map { URLQueryItem(name: $0.key, value: $0.value) }
+        guard
+            let query = request.query,
+            var urlComponents = URLComponents(url: self, resolvingAgainstBaseURL: false) else { return self }
+        
+        let queryItems: [URLQueryItem] = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        // When the initial url doesn't contain query items we set the request's items. Otherwise we append the
+        // items to the existing ones.
+        if urlComponents.queryItems == nil {
+            urlComponents.queryItems = queryItems
+        } else {
+            urlComponents.queryItems?.append(contentsOf: queryItems)
+        }
         return urlComponents.url ?? self
     }
 }
