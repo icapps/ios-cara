15fa6e13f15c17440be0d4fb70221a21cb95e015
Jelle Vandebeeck <jelle@fousa.be>
2019-03-03 18:56:39 +0100

    Pass data to the interceptor.

--- Sources/Configuration/Interceptor.swift
+++ Sources/Configuration/Interceptor.swift
@@ -13,9 +13,10 @@ public protocol Interceptor {
     /// This function is triggered when a response error is triggered.
     ///
     /// - parameter error: The error that triggers the interceptor.
+    /// - parameter data: The body's data object if available.
     /// - parameter retry: The block you should trigger to retry the failed request.
     ///
     /// - returns: Return if you want the response processing to be intercepted so that
     /// the normal flow (with completion handler) is stopped.
-    func intercept(_ error: ResponseError, retry: @escaping () -> Void) -> Bool
+    func intercept(_ error: ResponseError, data: Data?, retry: @escaping () -> Void) -> Bool
 }
