37eea8a4e86a895f0dd0a1cb23b238a379aa0848
Jelle Vandebeeck <jelle@fousa.be>
2019-04-26 19:18:26 +0200

    Add a simple Cara project.

---
+++ Cara/Cara/Cara.h
@@ -0,0 +1,19 @@
+//
+//  Cara.h
+//  Cara
+//
+//  Created by Jelle Vandebeeck on 26/04/2019.
+//  Copyright © 2019 iCapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cara.
+FOUNDATION_EXPORT double CaraVersionNumber;
+
+//! Project version string for Cara.
+FOUNDATION_EXPORT const unsigned char CaraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cara/PublicHeader.h>
+
+
