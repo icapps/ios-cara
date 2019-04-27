8038e1f03ab111052563428170537efce730f9d1
Jelle Vandebeeck <jelle@fousa.be>
2019-04-27 22:34:30 +0200

    Cleanup the cara.h file.

--- Cara/Cara/Cara.h
+++ Cara/Cara/Cara.h
@@ -8,12 +8,5 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Cara.
 FOUNDATION_EXPORT double CaraVersionNumber;
-
-//! Project version string for Cara.
 FOUNDATION_EXPORT const unsigned char CaraVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <Cara/PublicHeader.h>
-
-
