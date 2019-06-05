a1670e4c39cc5ed41a1b0a7b6925720ecfd8fd24
Jelle Vandebeeck <jelle@fousa.be>
2019-06-05 15:41:35 +0200

    Fux the target order.

--- Package.swift
+++ Package.swift
@@ -7,13 +7,13 @@ let package = Package(
     .library(name: "Cara", targets: ["Cara"])
   ],
   dependencies: [],
-  targets: [
-    .target(name: "Cara", dependencies: [], path: "Sources")
-  ],
   platforms: [
       .macOS(.v10_10),
       .iOS(.v10_0),
       .tvOS(.v10_0)
+  ],
+  targets: [
+    .target(name: "Cara", dependencies: [], path: "Sources")
   ],
   swiftLanguageVersions: [5]
 )
