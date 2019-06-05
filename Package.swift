f3882374513650e7fa56123fbea99d98499a5183
Jelle Vandebeeck <jelle@fousa.be>
2019-06-05 15:40:34 +0200

    Add platforms and language definitions.

--- Package.swift
+++ Package.swift
@@ -8,6 +8,12 @@ let package = Package(
   ],
   dependencies: [],
   targets: [
-        .target(name: "Cara", dependencies: [], path: "Sources")
-    ]
+    .target(name: "Cara", dependencies: [], path: "Sources")
+  ],
+  platforms: [
+      .macOS(.v10_10),
+      .iOS(.v10_0),
+      .tvOS(.v10_0)
+  ],
+  swiftLanguageVersions: [5]
 )
