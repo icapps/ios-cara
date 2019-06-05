89be60e550437c8a2ddcc5757e2e025fbd7df07e
Jelle Vandebeeck <jelle@fousa.be>
2019-06-05 15:42:36 +0200

    Fix the platform order.

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
   name: "Cara",
-  products: [
-    .library(name: "Cara", targets: ["Cara"])
-  ],
-  dependencies: [],
   platforms: [
       .macOS(.v10_10),
       .iOS(.v10_0),
       .tvOS(.v10_0)
   ],
+  products: [
+    .library(name: "Cara", targets: ["Cara"])
+  ],
+  dependencies: [],
   targets: [
     .target(name: "Cara", dependencies: [], path: "Sources")
   ],
