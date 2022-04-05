1b6401a7713a87441edb74ee7bee25782d7f90ee
Dylan Gyesbreghs <dgyesbreghs@gmail.com>
2022-04-05 23:00:43 +0200

    Make sure MockingJay is working again

--- Example/Pods/Mockingjay/Sources/Mockingjay/Builders.swift
+++ Example/Pods/Mockingjay/Sources/Mockingjay/Builders.swift
@@ -10,9 +10,17 @@ import Foundation
 
 // Collection of generic builders
 
+internal struct MockingjayFailure: Error {}
+
 /// Generic builder for returning a failure
-public func failure(_ error: NSError) -> (_ request: URLRequest) -> Response {
-  return { _ in return .failure(error) }
+public func failure(_ error: Error? = nil) -> (_ request: URLRequest) -> Response {
+  return { _ in
+    if let error = error {
+      return .failure(error)
+    }
+
+    return .failure(MockingjayFailure())
+  }
 }
 
 public func http(_ status:Int = 200, headers:[String:String]? = nil, download:Download=nil) -> (_ request: URLRequest) -> Response {
@@ -23,6 +31,19 @@ public func http(_ status:Int = 200, headers:[String:String]? = nil, download:Do
 
     return .failure(NSError(domain: NSExceptionName.internalInconsistencyException.rawValue, code: 0, userInfo: [NSLocalizedDescriptionKey: "Failed to construct response for stub."]))
   }
+}
+
+public func text(_ body: String, using encoding: String.Encoding, status: Int = 200, headers: [String: String]? = nil) -> (_ request: URLRequest) -> Response {
+  var headers = headers ?? [String:String]()
+  if headers["Content-Type"] == nil && encoding == .utf8 {
+    headers["Content-Type"] = "text/plain; charset=utf-8"
+  }
+
+  if let data = body.data(using: encoding) {
+    return http(status, headers: headers, download: .content(data))
+  }
+
+  return failure()
 }
 
 public func json(_ body: Any, status:Int = 200, headers:[String:String]? = nil) -> (_ request: URLRequest) -> Response {
@@ -44,5 +65,15 @@ public func jsonData(_ data: Data, status: Int = 200, headers: [String:String]? 
     }
     
     return http(status, headers: headers, download: .content(data))(request)
+  }
+}
+
+public func redirect(to url: URL, status: Int = 301, headers: [String:String]? = nil) -> (_ request: URLRequest) -> Response {
+  var headers = headers ?? [:]
+  headers["Location"] = url.absoluteString
+
+  let resoponse = HTTPURLResponse(url: url, statusCode: status, httpVersion: nil, headerFields: headers)!
+  return { request in
+    return .success(resoponse, .noContent)
   }
 }
